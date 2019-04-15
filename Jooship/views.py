b710ccf210f974f1692145dc5387c2d05ed9baac
Minkyu Yeo <woas23@naver.com>
2019-04-15 21:44:57 +0900

    [190415] 검색한 ticker detail view template 으로 redirection
    * 반영사항
      * 검색한 ticker 를 search view 함수로 전달한다.
      * 전달한 ticker 를 detail view template 로 재전달한다.
      * 재전달한 ticker 를 detail view template 에서 사용 가능하다.

--- Jooship/views.py
+++ Jooship/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from . import plots
 
@@ -15,6 +17,8 @@ class DetailView(TemplateView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(DetailView, self).get_context_data(**kwargs)
+        ticker = kwargs['stock_ticker']
+        print(ticker)
         context['revenue_plot'] = plots.get_revenue_plot()
         context['cash_flow_plot'] = plots.get_cash_flow_plot()
         context['eps_plot'] = plots.get_eps_plot()
@@ -22,10 +26,16 @@ class DetailView(TemplateView):
         return context
 
 
+@csrf_exempt
 def search_ticker(request):
-    ticker = request.POST.get('ticker', False)
-    print(ticker)
-    return render(request, 'Jooship/detail.html')
-
-# class SearchView(TemplateView):
-#     pass
+    # ticker = request.POST.get('ticker', False)
+    try:
+        ticker = request.POST.get('ticker', False)
+    except KeyError:
+        print("error")
+        return HttpResponseRedirect(reverse('Jooship:index'))
+    else:
+        # print(ticker)
+        pass
+    return HttpResponseRedirect(reverse('Jooship:detail', kwargs={'stock_ticker': ticker}))
+# End of File
